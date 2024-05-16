4eefb141a72f767f0303cd3bbb270f70f3f77ff0
alexis-82 <alessioabrugiati@gmail.com>
2024-05-16 17:44:14 +0200

    aggiunto reactstrap+function

--- its-react-app/src/App.jsx
+++ its-react-app/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { User } from './User'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +21,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <button color='success' onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
