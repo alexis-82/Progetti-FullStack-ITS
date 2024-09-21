11b46c425ec1cef4110ecdf9f5560ddb461264e9
alexis-82 <giobbia82@gmail.com>
2024-09-21 18:50:15 +0200

    update

--- gestione_clienti/server/server.js
+++ gestione_clienti/server/server.js
@@ -14,7 +14,7 @@ const pool = mysql.createPool({
     host: '192.168.1.200',
     user: 'root',
     password: 'admin',
-    database: 'gestione_utenti',
+    database: 'gestione_clienti',
     port: '3306'
 });
 
